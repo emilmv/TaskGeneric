b0e8ddd00e232b43300b578e28ea9de4844ec7ef
emilmv <emilnm@code.edu.az>
2024-03-19 09:19:20 +0400

    Add project files.

---
+++ TaskGeneric/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Student student2 = new Student();
+            Student student3 = new Student();
+            student.showInfo();
+            student2.showInfo();
+            student3.showInfo();
+        }
+    }
+}
---
+++ TaskGeneric/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace TaskGeneric
+{
+    internal class Student
+    {
+        static int _id;
+        public int Id { get;}
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public byte Age { get; set; }
+        public double Point { get; set; }
+
+        public static bool operator >(Student a, Student b)
+        {
+            return a.Point > b.Point;
+        }
+        public static bool operator <(Student a, Student b)
+        {
+            return a.Point < b.Point;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("ID: "+Id);
+            Console.WriteLine("NAME: "+Name);
+            Console.WriteLine("SURNAME: "+Surname);
+            Console.WriteLine("AGE: "+Age);
+            Console.WriteLine("POINT:"+Point);
+        }
+        public Student(string name, string surname, double point)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            Surname = surname;
+            Point= point;
+        }
+
+    }
+}
